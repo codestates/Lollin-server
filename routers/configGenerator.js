f03935d545de38fed839f6a5ae0249a3149a421c
leehojun0314 <dlghwns0314@naver.com>
2021-05-18 20:56:47 +0900

    userinfo, activegame info

---
+++ routers/configGenerator.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+
+const configGenerator = function (apiType, param) {
+	switch (apiType) {
+		case 'activegame': {
+			console.log('param: ', param);
+			console.log('encode url');
+			console.log(encodeURI(param));
+			return {
+				method: 'get',
+				url: `https://kr.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${encodeURI(
+					param,
+				)}`,
+				headers: {
+					'User-Agent':
+						'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+					'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+					'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
+					Origin: 'https://developer.riotgames.com',
+					'X-Riot-Token': process.env.Riot_Token,
+				},
+			};
+		}
+		case 'userinfo': {
+			console.log('param: ', param);
+			console.log('encode url');
+			console.log(encodeURI(param));
+			return {
+				method: 'get',
+				url: `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodeURI(
+					param,
+				)}`,
+				headers: {
+					'User-Agent':
+						'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+					'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+					'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
+					Origin: 'https://developer.riotgames.com',
+					'X-Riot-Token': process.env.Riot_Token,
+				},
+			};
+		}
+		default: {
+			break;
+		}
+	}
+};
+
+module.exports = configGenerator;
--- routers/controller/utilsController.js
+++ routers/controller/utilsController.js
@@ -1,6 +1,28 @@
 const router = require('express').Router();
-
-router.get('/', (req, res) => {
-	res.send('utils');
+const axios = require('axios');
+const configGenerator = require('../configGenerator');
+router.get('/search?', (req, res) => {
+	console.log(req.query);
+	axios(configGenerator('userinfo', req.query.name))
+		.then((response) => {
+			console.log('response.data: ');
+			console.log(response.data);
+			axios(configGenerator('activegame', response.data.id))
+				.then((response) => {
+					console.log(response);
+					res.status(200).send(response.data);
+				})
+				.catch((err) => {
+					console.log(err);
+					res.status(404).send({
+						message: 'Player is not participating any game',
+						err: err,
+					});
+				});
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).send(err);
+		});
 });
 module.exports = router;
