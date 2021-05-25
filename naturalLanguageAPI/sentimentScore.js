84cdf4913b654ed262b2f37cd1d08996db882124
whwnddnwhwnd <whwnddnwhwnd@gmail.com>
2021-05-25 14:35:15 +0900

    google API

---
+++ naturalLanguageAPI/sentimentScore.js
@@ -0,0 +1,23 @@
+const nL = async (comment) => {
+  // Imports the Google Cloud client library
+  const language = require('@google-cloud/language');
+
+  // Instantiates a client
+  const client = new language.LanguageServiceClient();
+
+  // The text to analyze
+  const text = comment;
+
+  const document = {
+    content: text,
+    type: 'PLAIN_TEXT',
+  };
+
+  // Detects the sentiment of the text
+  const [result] = await client.analyzeSentiment({ document: document });
+  const sentiment = result.documentSentiment;
+
+  return sentiment.score;
+};
+
+module.exports = nL;
