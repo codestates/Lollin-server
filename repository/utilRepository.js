0591da2d2144680936daaea267bed13935caad8d
whwnddnwhwnd <whwnddnwhwnd@gmail.com>
2021-05-25 17:43:34 +0900

    update

--- repository/utilRepository.js
+++ repository/utilRepository.js
@@ -14,26 +14,29 @@ const utilRepository = {
       const collection = mongoDB.db('lollin').collection('comments');
       let scoreSum = 0;
       const { comments } = await collection.findOne({ nickname: nickname });
-      for (let i = 0; i < comments.length; i++) {
-        scoreSum += await sentimentScore(comments[i]);
-      }
-      mongoDB.close();
-      let score = parseFloat(scoreSum / comments.length).toFixed(2);
-      console.log(score);
-      if (0.6 <= score && score <= 1) {
-        score = scoreResult[4];
-      } else if (0.2 <= score && score < 0.6) {
-        score = scoreResult[3];
-      } else if (-0.2 <= score && score < 0.2) {
-        score = scoreResult[2];
-      } else if (-0.6 <= score && score < -0.2) {
-        score = scoreResult[1];
-      } else if (-1 <= score && score < -0.6) {
-        score = scoreResult[0];
+      if (comments) {
+        for (let i = 0; i < comments.length; i++) {
+          scoreSum += await sentimentScore(comments[i]);
+        }
+        mongoDB.close();
+        let score = parseFloat(scoreSum / comments.length).toFixed(2);
+        if (0.6 <= score && score <= 1) {
+          score = scoreResult[4];
+        } else if (0.2 <= score && score < 0.6) {
+          score = scoreResult[3];
+        } else if (-0.2 <= score && score < 0.2) {
+          score = scoreResult[2];
+        } else if (-0.6 <= score && score < -0.2) {
+          score = scoreResult[1];
+        } else if (-1 <= score && score < -0.6) {
+          score = scoreResult[0];
+        } else {
+          score = '평가 결과가 없습니다.';
+        }
+        matchResult.data.score = score;
       } else {
-        score = '평가 결과가 없습니다';
+        matchResult.data.score = '평가 결과가 없습니다.';
       }
-      matchResult.data.score = score;
       HttpResponse.status(200).send(matchResult.data);
     });
   },
