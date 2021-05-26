52a3734f4109e731a553787d07f7e2301a4e7cd8
leehojun0314 <dlghwns0314@naver.com>
2021-05-26 17:58:58 +0900

    details: lore, tips, tags

--- routers/controller/championsController.js
+++ routers/controller/championsController.js
@@ -16,12 +16,9 @@ router.get('/detail', (req, res) => {
 	axios
 		.get('https://ddragon.leagueoflegends.com/api/versions.json')
 		.then((response) => {
-			console.log(response.data[0]);
 			return response.data[0];
 		})
 		.then((version) => {
-			console.log('version: ', version);
-			console.log('id: ', champId);
 			return new Promise((resolve, reject) => {
 				axios
 					.get(
@@ -31,41 +28,32 @@ router.get('/detail', (req, res) => {
 					)
 					.then((response2) => {
 						if (response2.data) {
-							console.log(response2.data);
 							resolve({ champdata: response2.data, version: version });
 						} else {
 							reject('invalid champion id');
 						}
 					})
 					.catch((err) => {
-						console.log('rejected here');
 						reject(err);
 					});
 			});
 		})
 		.then(({ champdata, version }) => {
-			console.log('version: ', version);
 			let key = champdata.data[champId].key;
-			console.log('key before process: ', key);
-			console.log(key.length);
 			switch (key.length) {
 				case 1: {
-					console.log('this is 1');
 					key = '000' + key;
 					break;
 				}
 				case 2: {
-					console.log('this is 2');
 					key = '00' + key;
 					break;
 				}
 				case 3: {
-					console.log('this is 3');
 					key = '0' + key;
 					break;
 				}
 			}
-			console.log('key: ', key);
 
 			let imgs = [
 				`http://ddragon.leagueoflegends.com/cdn/${version}/img/passive/${champdata.data[champId].passive.image.full}`,
@@ -79,7 +67,6 @@ router.get('/detail', (req, res) => {
 					`http://ddragon.leagueoflegends.com/cdn/${version}/img/spell/${skillId.id}.png`,
 				);
 			}
-
 			let data = {
 				id: champId,
 				img: `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champId}_0.jpg`,
@@ -92,11 +79,17 @@ router.get('/detail', (req, res) => {
 					`https://d28xe8vt774jo5.cloudfront.net/champion-abilities/${key}/ability_${key}_E1.webm`,
 					`https://d28xe8vt774jo5.cloudfront.net/champion-abilities/${key}/ability_${key}_R1.webm`,
 				],
+				lore: champdata.data[champId].lore,
+				allytips: champdata.data[champId].allytips,
+				enemytips: champdata.data[champId].enemytips,
+				tags: champdata.data[champId].tags,
 			};
+			console.log(data);
+			console.log(champId);
+			console.log(version);
 			res.send({ data: data });
 		})
 		.catch((err) => {
-			console.log(err);
 			res.status(404).send(err);
 		});
 	// res.send('detail');
